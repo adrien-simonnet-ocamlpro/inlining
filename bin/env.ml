1e847a6d0fee2f3f7c317a70d245bc46cfacfd5d
Adrien Simonnet <adrien.simonnet@etu.univ-sorbonne.fr>
2023-05-04 18:00:46 +0200

    env

--- bin/env.ml
+++ bin/env.ml
@@ -13,18 +13,6 @@ let get env var =
        ^ " ].")
 ;;
 
-let get_cont env var =
-  match List.find_opt (fun (var', _, _, _) -> var = var') env with
-  | Some (_, args, v, env') -> args, v, env'
-  | None ->
-    failwith
-      ("k"
-       ^ string_of_int var
-       ^ " not found in "
-       ^ List.fold_left (fun str (x, _, _, _) -> str ^ " k" ^ string_of_int x) "[" env
-       ^ " ].")
-;;
-
 let get2 env var =
   match List.find_opt (fun (var', _) -> var = var') env with
   | Some (_, v) -> v
@@ -37,8 +25,6 @@ let get2 env var =
        ^ " ].")
 ;;
 
-let has3 env var = List.exists (fun (_, var') -> var = var') env
-
 let get3 env var =
   match List.find_opt (fun (va, _) -> var = va) env with
   | Some (v, _) -> v
@@ -50,6 +36,55 @@ let get3 env var =
        ^ List.fold_left (fun str (x, _) -> str ^ " x" ^ (string_of_int x)) "[" env
        ^ " ].")
 ;;
+
+
+let has_var env var = List.exists (fun (var', _) -> var = var') env
+
+let has_value env value = List.exists (fun (_, value') -> value = value') env
+
+let get_value env var =
+  match List.find_opt (fun (v, _) -> var = v) env with
+  | Some (_, v) -> v
+  | None ->
+    failwith
+      (var
+       ^ " not found in "
+       ^ List.fold_left (fun str (x, _) -> str ^ x) "[" env
+       ^ " ].")
+;;
+
+let get_var env var =
+  match List.find_opt (fun (_, v) -> var = v) env with
+  | Some (v, _) -> v
+  | None ->
+    failwith
+      ("_"
+       ^ (string_of_int var)
+       ^ " not found in "
+       ^ List.fold_left (fun str (_, x) -> str ^ " _" ^ (string_of_int x)) "[" env
+       ^ " ].")
+;;
+
+
+
+
+let get_cont env var =
+  match List.find_opt (fun (var', _, _, _) -> var = var') env with
+  | Some (_, args, v, env') -> args, v, env'
+  | None ->
+    failwith
+      ("k"
+       ^ string_of_int var
+       ^ " not found in "
+       ^ List.fold_left (fun str (x, _, _, _) -> str ^ " k" ^ string_of_int x) "[" env
+       ^ " ].")
+;;
+
+
+
+let has3 env var = List.exists (fun (_, var') -> var = var') env
+
+
 
 let get_name var = List.find_opt (fun (_, v) -> var = v)
 
