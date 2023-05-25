7f8c5db3bf252f9535799fef034315c0d324fd6e
Adrien Simonnet <adrien.simonnet@etu.univ-sorbonne.fr>
2023-05-25 17:56:54 +0200

    stack analysis

--- bin/analysis.ml
+++ bin/analysis.ml
@@ -25,6 +25,12 @@ type cont = Cps.cont
 let get = Env.get2
 let has = Env.has
 
+let get2 env var =
+  match List.find_opt (fun (var', _) -> var = var') env with
+  | Some (_, v) -> v
+  | None -> assert false
+;;
+
 let get_cont = Cps.get_cont
 
 let map_values = Cps.map_values
@@ -32,6 +38,19 @@ let map_values = Cps.map_values
 let map_args (args: var list) (env: env_domain) = List.map (fun arg -> get env arg) args
 
 let map_stack (stack: stack) (env: env_domain) = List.map (fun (k'', args') -> k'', map_args args' env) stack
+
+let rec cherche_motif motif liste =
+  match motif, liste with
+  | [], _ -> true
+  | m'::motif', l'::liste' when m' = l' -> cherche_motif motif' liste'
+  | _, _ -> false
+
+let rec cherche_periode periode liste =
+  match liste with
+  | [] -> false
+  | l::liste' -> cherche_motif (periode@[l]) liste' || cherche_periode (periode@[l]) liste'
+
+let join_stack old_stack new_stack = if cherche_periode [] new_stack then old_stack else new_stack
 
 let analysis_prim (prim : prim) args (env : env_domain) : value_domain =
   match prim, args with
@@ -59,22 +78,22 @@ let analysis_named (named : named) (env : env_domain) : value_domain =
 let rec analysis_cont (cps: expr) (stack: ((pointer * value_domain list) list)) (env: (address * value_domain) list) : (pointer * value_domain list * ((pointer * value_domain list) list)) list =
   match cps with
   | Let (var, named, expr) -> let value = analysis_named named env in analysis_cont expr stack ((var, value)::env)
-  | Apply_cont (k', args, stack') -> [k', map_args args env, (map_stack stack' env)@stack]
+  | Apply_cont (k', args, stack') -> [k', map_args args env, (join_stack stack ((map_stack stack' env)@stack))]
   | If (var, (kt, argst), (kf, argsf), stack') -> 
     if has env var then begin
       match get env var with
-      | Int_domain i when Int_domain.is_singleton i && Int_domain.get_singleton i = 0 -> [kt, map_args argst env, (map_stack stack' env)@stack]
-      | Int_domain i when Int_domain.is_singleton i && Int_domain.get_singleton i != 0 -> [kf, map_args argsf env, (map_stack stack' env)@stack]
-      | Int_domain _ -> [kt, map_args argst env, (map_stack stack' env)@stack; kf, map_args argsf env, (map_stack stack' env)@stack]
+      | Int_domain i when Int_domain.is_singleton i && Int_domain.get_singleton i = 0 -> [kt, map_args argst env, (join_stack stack ((map_stack stack' env)@stack))]
+      | Int_domain i when Int_domain.is_singleton i && Int_domain.get_singleton i != 0 -> [kf, map_args argsf env, (join_stack stack ((map_stack stack' env)@stack))]
+      | Int_domain _ -> [kt, map_args argst env, (join_stack stack ((map_stack stack' env)@stack)); kf, map_args argsf env, (join_stack stack ((map_stack stack' env)@stack))]
       | _ -> failwith "invalid type"
-    end else [kt, map_args argst env, (map_stack stack' env)@stack; kf, map_args argsf env, (map_stack stack' env)@stack]
+    end else [kt, map_args argst env, (join_stack stack ((map_stack stack' env)@stack)); kf, map_args argsf env, (join_stack stack ((map_stack stack' env)@stack))]
   | Return x -> begin match stack with
       | [] -> []
       | (k, args)::stack' -> [k, (get env x)::args, stack']
     end
   | Call (x, args, stack') -> begin
     match get env x with
-    | Pointer_domain d -> List.map (fun k -> k, map_args args env, (map_stack stack' env)@stack) (Pointer_domain.to_list d)
+    | Pointer_domain d -> List.map (fun k -> k, map_args args env, (join_stack stack ((map_stack stack' env)@stack))) (Pointer_domain.to_list d)
     | _ -> failwith "invalid type" end
 
 
@@ -90,24 +109,28 @@ and join_env (old_env: value_domain list) (new_env: value_domain list): value_do
 
 
 
-
-
-let rec analysis (conts: (pointer * value_domain list * ((pointer * value_domain list) list)) list) (prog: cont) (map: (value_domain list) Analysis.t) =
+let rec analysis (conts: (pointer * value_domain list * ((pointer * value_domain list) list)) list) (prog: cont) (map: ((((address * value_domain list) list) * value_domain list) list) Analysis.t) : (value_domain list) Analysis.t =
   match conts with
-  | [] -> map
+  | [] -> Analysis.map (fun contexts -> List.fold_left (fun acc (_, new_env) -> if acc = [] then new_env else join_env acc new_env) [] contexts) map
   | (k, env, stack)::conts' ->
     if Analysis.mem k map then 
-      let old_env = Analysis.find k map in
-      let new_env = join_env old_env env in
-      if new_env = old_env
-        then analysis conts' prog map
-        else let args, cont = get_cont prog k in
-          let next_conts = analysis_cont cont stack (map_values args env) in
-          analysis (conts'@next_conts) prog (Analysis.add k new_env map)
+      let old_context = Analysis.find k map in
+      if has old_context stack then
+        let old_env = get2 old_context stack in
+        let new_env = join_env old_env env in
+        if new_env = old_env
+          then analysis conts' prog map
+          else let args, cont = get_cont prog k in
+            let next_conts = analysis_cont cont stack (map_values args env) in
+            analysis (conts'@next_conts) prog (Analysis.add k ((stack,new_env)::old_context) map)
+      else
+        let args, cont = get_cont prog k in
+        let next_conts = analysis_cont cont stack (map_values args env) in
+        analysis (conts'@next_conts) prog (Analysis.add k ((stack,env)::old_context) map)
     else
       let args, cont = get_cont prog k in
       let next_conts = analysis_cont cont stack (map_values args env) in
-      analysis (conts'@next_conts) prog (Analysis.add k env map)
+      analysis (conts'@next_conts) prog (Analysis.add k [stack,env] map)
 
 let start_analysis prog args = analysis [0, args, []] prog (Analysis.empty)
 
