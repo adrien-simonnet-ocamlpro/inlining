0ebbea441796d48c21177f6f54d06c811d8b058e
Adrien Simonnet <adrien.simonnet@etu.univ-sorbonne.fr>
2023-05-31 17:36:28 +0200

    main analysis

--- bin/main.ml
+++ bin/main.ml
@@ -41,9 +41,8 @@ let _ =
       else (
         let cps, subs, fv, cont = Ast.to_cps (Cps.End) [] ast 0 (Return 0) [] in
         let cont' = Cps.Let_cont (0, fv, cps, cont) in
-        let analysis = Analysis.start_analysis cont' (List.map (fun _ -> Analysis.Int_domain (Int_domain.top)) fv) in
-        Analysis.pp_analysis (Format.std_formatter) analysis;
-        let cps2 = if !prop then Propagation.propagation_cont cont' cont' analysis else cont' in
+        let cps2 = if !prop then let analysis = Analysis.start_analysis cont' (List.map (fun _ -> Analysis.Int_domain (Int_domain.top)) fv) in
+        Analysis.pp_analysis (Format.std_formatter) analysis; Propagation.propagation_cont cont' cont' analysis else cont' in
         let cps3 =
           if !unused_vars
           then let cps, _ = Cleaner.elim_unused_vars_cont (Array.make 1000 0) cps2 in cps
