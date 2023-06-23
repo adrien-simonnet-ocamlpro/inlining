f4249c716e7c2aaee7c13d47d1104176c7c9b961
Adrien Simonnet <adrien.simonnet@etu.univ-sorbonne.fr>
2023-06-23 17:27:52 +0200

    Aps

---
+++ bin/aps.ml
@@ -0,0 +1,177 @@
+type var = int
+type pointer = int
+type tag = int
+type frame = pointer * var list
+
+module VarMap = Map.Make (Int)
+
+type prim = Asm.prim
+
+type named =
+| Prim of prim * var list
+| Var of var
+| Tuple of var list
+| Get of var * int
+| Closure of pointer * var list
+| Constructor of tag * var list
+
+and expr =
+| Let of var * named * expr
+| Apply_cont of pointer * var list
+| Call_direct of pointer * var * var list * frame
+| Call_indirect of var * var list * frame
+| If of var * (int * pointer * var list) list * (pointer * var list)
+| Match_pattern of var * (int * pointer * var list) list * (pointer * var list)
+| Return of var
+
+type cont =
+| Let_cont of pointer * var list * expr * cont
+| Let_clos of pointer * var list * var list * expr * cont
+| End
+
+let gen_name (var: var) (subs: string VarMap.t): string =
+  match VarMap.find_opt var subs with
+  | Some str -> str ^ "_" ^ (string_of_int var)
+  | None -> "_" ^ (string_of_int var)
+
+let rec pp_args ?(subs = (VarMap.empty: string VarMap.t)) ?(empty=(" ": string)) ?(split=(" ": string)) (fmt: Format.formatter) (args: var list): unit =
+  match args with
+  | [] -> Format.fprintf fmt "%s" empty
+  | [ arg ] -> Format.fprintf fmt "%s" (gen_name arg subs)
+  | arg :: args' -> Format.fprintf fmt "%s%s%a" (gen_name arg subs) split (pp_args ~split ~empty ~subs) args'
+
+let pp_prim (subs: string VarMap.t) (fmt: Format.formatter) (prim : prim) (args: var list): unit =
+  match prim, args with
+  | Const x, _ -> Format.fprintf fmt "Int %d" x
+  | Add, x1 :: x2 :: _ -> Format.fprintf fmt "add %s %s" (gen_name x1 subs) (gen_name x2 subs)
+  | Sub, x1 :: x2 :: _ -> Format.fprintf fmt "sub %s %s" (gen_name x1 subs) (gen_name x2 subs)
+  | Print, x1 :: _ -> Format.fprintf fmt "print %s" (gen_name x1 subs)
+  | _ -> failwith "invalid args"  
+
+let pp_named (subs: string VarMap.t) (fmt: Format.formatter) named =
+match named with
+| Prim (prim, args) -> pp_prim subs fmt prim args
+| Var x -> Format.fprintf fmt "%s" (gen_name x subs)
+| Tuple (args) -> Format.fprintf fmt "Tuple [%a]" (pp_args ~split: "; " ~subs ~empty:"") args
+| Get (record, pos) -> Format.fprintf fmt "Get (%s, %d)" (gen_name record subs) pos
+| Closure (k, env) -> Format.fprintf fmt "Closure (f%d, %a)" k (pp_args ~split:" " ~subs ~empty: "()") env
+| Constructor (tag, env) -> Format.fprintf fmt "Constructor (%d, [%a])" tag (pp_args ~split: "; " ~subs ~empty: "") env
+
+let rec pp_expr (subs: string VarMap.t) (fmt: Format.formatter) (cps : expr) : unit =
+  match cps with
+  | Let (var, named, expr) -> Format.fprintf fmt "\tlet %s = %a in\n%a" (gen_name var subs) (pp_named subs) named (pp_expr subs) expr
+  | Apply_cont (k, args) -> Format.fprintf fmt "\tk%d %a" k (pp_args ~subs ~split: " " ~empty: "") args
+  | If (var, matchs, (kf, argsf)) -> Format.fprintf fmt "\tmatch %s with%s | _ -> k%d %a" (gen_name var subs) (List.fold_left (fun acc (n, kt, argst) -> acc ^ (Format.asprintf "| Int %d -> k%d %a " n kt (pp_args ~subs ~empty: "()" ~split: " ") argst)) " " matchs) kf (pp_args ~subs ~empty: "()" ~split: " ") argsf
+  | Match_pattern (var, matchs, (kf, argsf)) -> Format.fprintf fmt "\tmatch %s with%s | _ -> k%d %a" (gen_name var subs) (List.fold_left (fun acc (n, kt, argst) -> acc ^ (Format.asprintf "| Int %d -> f%d %a " n kt (pp_args ~subs ~empty: "()" ~split: " ") argst)) " " matchs) kf (pp_args ~subs ~empty: "()" ~split: " ") argsf
+  | Return x -> Format.fprintf fmt "\t%s" (gen_name x subs)
+  | Call_direct (k', x, args, (k, kargs)) -> Format.fprintf fmt "\tk%d (k%d %s %a) %a" k k' (gen_name x subs) (pp_args ~split:" " ~subs ~empty: "()") args (pp_args ~split:" " ~subs ~empty: "()") kargs
+  | Call_indirect (x, args, (k, kargs)) -> Format.fprintf fmt "\tk%d (%s %a) %a" k (gen_name x subs) (pp_args ~split:" " ~subs ~empty: "()") args (pp_args ~split:" " ~subs ~empty: "()") kargs
+
+
+let rec pp_cont ?(join = "let rec") (subs: string VarMap.t) (fmt: Format.formatter) (cps : cont) : unit =
+  match cps with
+  | Let_cont (k, args, e1, cont) -> Format.fprintf fmt "%s k%d %a =\n%a\n%a" join k (pp_args ~subs ~empty: "()" ~split: " ") args (pp_expr subs) e1 (pp_cont ~join: "and" subs) cont
+  | Let_clos (k, env, args, e1, cont) -> Format.fprintf fmt "%s f%d %a %a =\n%a\n%a" join k (pp_args ~subs ~empty: "()" ~split: " ") env (pp_args ~subs ~empty: "()" ~split: " ") args (pp_expr subs) e1 (pp_cont ~join: "and" subs) cont
+  | End -> Format.fprintf fmt "%!"
+
+let update_var (var: var) (alias: var VarMap.t): var = if VarMap.mem var alias then VarMap.find var alias else var
+let update_vars (vars: var list) (alias: var VarMap.t): var list = List.map (fun var -> update_var var alias) vars
+
+let clean_named (named: named) (alias: var VarMap.t): named =
+  match named with
+  | Prim (prim, vars) -> Prim (prim, update_vars vars alias)
+  | Var var -> Var (update_var var alias)
+  | Tuple vars -> Tuple (update_vars vars alias)
+  | Get (record, pos) -> Get (update_var record alias, pos)
+  | Closure (k, vars) -> Closure (k, update_vars vars alias)
+  | Constructor (tag, vars) -> Constructor (tag, update_vars vars alias)
+
+let rec clean_expr (expr: expr) (alias: var VarMap.t): expr =
+  match expr with
+  | Let (var, Var var', expr') -> clean_expr expr' (VarMap.add var var' alias)
+  | Let (var, named, expr) -> Let (var, clean_named named alias, clean_expr expr alias)
+  | Apply_cont (k, args) -> Apply_cont (k, update_vars args alias)
+  | If (var, matchs, (kf, argsf)) -> If (update_var var alias, List.map (fun (n, k, args) -> n, k, update_vars args alias) matchs, (kf, update_vars argsf alias))
+  | Match_pattern (pattern_id, matchs, (kf, argsf)) -> Match_pattern (update_var pattern_id alias, List.map (fun (n, k, args) -> n, k, update_vars args alias) matchs, (kf, update_vars argsf alias))
+  | Return var -> Return (update_var var alias)
+  | Call_direct (k', x, args, (k, kargs)) -> Call_direct (k', update_var x alias, update_vars args alias, (k, update_vars kargs alias))
+  | Call_indirect (x, args, (k, kargs)) -> Call_indirect (update_var x alias, update_vars args alias, (k, update_vars kargs alias))
+
+let rec clean_cont (cps: cont): cont =
+  match cps with
+  | Let_cont (k', args', e1, e2) -> Let_cont (k', args', clean_expr e1 (VarMap.empty), clean_cont e2)
+  | Let_clos (k', body_free_variables, args', e1, e2) -> Let_clos (k', body_free_variables, args', clean_expr e1 (VarMap.empty), clean_cont e2)
+  | End -> End
+
+let inc (vars: var Seq.t): var * var Seq.t =
+  match Seq.uncons vars with
+  | None -> assert false
+  | Some (i, vars') -> i, vars'
+
+let rec named_to_asm (var: var) (named: named) (expr: expr) (vars: var Seq.t): Asm.expr * var Seq.t =
+  match named with
+  | Prim (prim, args) -> begin
+      let asm, vars = expr_to_asm expr vars in
+      Let (var, Prim (prim, args), asm), vars
+    end
+  | Var x -> begin
+      let asm, vars = expr_to_asm expr vars in
+      Let (var, Var x, asm), vars
+    end
+  | Tuple args -> begin
+      let asm, vars = expr_to_asm expr vars in
+      Let (var, Tuple args, asm), vars
+    end
+  | Get (record, pos) -> begin
+      let asm, vars = expr_to_asm expr vars in
+      Let (var, Get (record, pos), asm), vars
+    end
+  | Closure (k, env) -> begin
+      let k_id, vars = inc vars in
+      let env_id, vars = inc vars in
+      let asm, vars = expr_to_asm expr vars in
+      Let (k_id, Pointer k, Let (env_id, Tuple env, Let (var, Tuple [k_id; env_id], asm))), vars
+    end
+  | Constructor (tag, env) -> begin
+      let tag_id, vars = inc vars in
+      let env_id, vars = inc vars in
+      let asm, vars = expr_to_asm expr vars in
+      Let (tag_id, Prim (Const tag, []), Let (env_id, Tuple env, Let (var, Tuple [tag_id; env_id], asm))), vars
+    end
+
+and expr_to_asm (cps: expr) (vars: var Seq.t): Asm.expr * int Seq.t =
+  match cps with
+  | Let (var, named, expr) -> named_to_asm var named expr vars
+  | Apply_cont (k, args) -> Apply_direct (k, args, []), vars
+  | If (var, matchs, (kf, argsf)) -> If (var, matchs, (kf, argsf), []), vars
+  | Match_pattern (cons, matchs, (kf, argsf)) -> begin
+      let tag_id, vars = inc vars in
+      let payload_id, vars = inc vars in
+      Asm.Let (tag_id, Get (cons, 0), (Asm.Let (payload_id, Get (cons, 1), If (tag_id, List.map (fun (n, k, args) -> (n, k, payload_id :: args)) matchs, (kf, argsf), [])))), vars
+    end
+  | Return var -> Return var, vars
+  | Call_direct (k_id, clos, args, frame) -> begin
+      let env_id, vars = inc vars in
+      Let (env_id, Get (clos, 1), Apply_direct (k_id, env_id :: args, [frame])), vars
+    end
+  | Call_indirect (clos, args, frame) -> begin
+      let k_id, vars = inc vars in
+      let env_id, vars = inc vars in
+      Let (k_id, Get (clos, 0), Let (env_id, Get (clos, 1), Apply_indirect (k_id, env_id :: args, [frame]))), vars
+    end
+
+let rec cont_to_asm (cps: cont) (vars: var Seq.t): Asm.cont * var Seq.t =
+  match cps with
+  | Let_cont (k', args', e1, e2) -> begin
+      let asm1, vars = expr_to_asm e1 vars in
+      let asm2, vars = cont_to_asm e2 vars in
+      Let_cont (k', args', asm1, asm2), vars
+    end
+  | Let_clos (k', body_free_variables, args', e1, e2) -> begin
+      let environment_id, vars = inc vars in
+      let asm1, vars = expr_to_asm e1 vars in
+      let asm2, vars = cont_to_asm e2 vars in
+      let body = List.fold_left (fun cps' (pos, body_free_variable) -> Asm.Let (body_free_variable, Asm.Get (environment_id, pos), cps')) asm1 (List.mapi (fun i fv -> i, fv) body_free_variables) in
+      Let_cont (k', environment_id :: args', body, asm2), vars
+    end
+  | End -> End, vars
--- bin/propagation.ml
+++ bin/propagation.ml
@@ -1,78 +1,101 @@
 
-type prim = Cps.prim
-type named = Cps.named
-type pointer = Cps.pointer
-type expr = Cps.expr
-type address = Cps.address
-type cont = Cps.cont
+type prim = Aps.prim
+type named = Aps.named
+type pointer = Aps.pointer
+type expr = Aps.expr
+type address = Aps.pointer
+type cont = Aps.cont
 
 type value_domain = Analysis.value_domain
-type env_domain = Analysis.env_domain
+module Allocations = Analysis.Allocations
 
-let get = Env.get2
-let has = Env.has
+let get = Analysis.get
+let has = Analysis.has
 
-let get_cont = Cps.get_cont
 
-let rec propagation_prim (prim : prim) args (env : env_domain) : named * value_domain =
+module Values = Analysis.Values
+
+let map_args2 = Analysis.map_args2
+let join_allocs = Analysis.join_allocs
+
+let rec propagation_prim (prim : prim) args (env: (address * Values.t) list) (allocations: value_domain Allocations.t): named * value_domain option =
   match prim, args with
-  | Const x, args' -> Prim (Const x, args'), Int_domain (Int_domain.singleton x)
-  | Add, x1 :: x2 :: args' -> begin match get env x1, get env x2 with
-    | Int_domain d1, Int_domain d2 when Int_domain.is_singleton d1 && Int_domain.is_singleton d2 -> let x = ((Int_domain.get_singleton d1) + Int_domain.get_singleton d2) in Prim (Const x, []), Int_domain (Int_domain.singleton x)
-    | Int_domain _, Int_domain _ -> Prim (Add, x1 :: x2 :: args'), Int_domain (Int_domain.top)
-    | _ -> failwith "invalid type"
-  end
-  | Sub, x1 :: x2 :: args' -> begin match get env x1, get env x2 with
-    | Int_domain d1, Int_domain d2 when Int_domain.is_singleton d1 && Int_domain.is_singleton d2 -> let x = ((Int_domain.get_singleton d1) - Int_domain.get_singleton d2) in Prim (Const x, []), Int_domain (Int_domain.singleton x)
-    | Int_domain _, Int_domain _ -> Prim (Add, x1 :: x2 :: args'), Int_domain (Int_domain.top)
-    | _ -> failwith "invalid type"
-  end
-  | Print, _ :: _ -> Prim (Print, args), Int_domain (Int_domain.top)
-  | _ -> failwith "invalid args"
+  | Const x, _ -> Prim (prim, args), Some (Int_domain (Int_domain.singleton x))
+  | Add, x1 :: x2 :: _ -> begin match get env x1 allocations, get env x2 allocations with
+    | Some (Int_domain d1), Some (Int_domain d2) when Int_domain.is_singleton d1 && Int_domain.is_singleton d2 -> Prim (Const ((Int_domain.get_singleton d1) + (Int_domain.get_singleton d2)), args), Some (Int_domain (Int_domain.singleton ((Int_domain.get_singleton d1) + (Int_domain.get_singleton d2))))
+    | Some (Int_domain _), Some (Int_domain _) -> Prim (prim, args), Some (Int_domain (Int_domain.top))
+    | Some (Int_domain _), None | None, Some (Int_domain _) | None, None -> Prim (prim, args), Some (Int_domain (Int_domain.top))
+    | _ -> assert false
+    end
+  | Sub, x1 :: x2 :: _ -> begin match get env x1 allocations, get env x2 allocations with
+    | Some (Int_domain d1), Some (Int_domain d2) when Int_domain.is_singleton d1 && Int_domain.is_singleton d2 -> Prim (Const ((Int_domain.get_singleton d1) - (Int_domain.get_singleton d2)), args), Some (Int_domain (Int_domain.singleton ((Int_domain.get_singleton d1) - (Int_domain.get_singleton d2))))
+    | Some (Int_domain _), Some (Int_domain _) -> Prim (prim, args), Some (Int_domain (Int_domain.top))
+    | Some (Int_domain _), None | None, Some (Int_domain _) | None, None -> Prim (prim, args), Some (Int_domain (Int_domain.top))
+    | _ -> assert false
+    end
+  | Print, _ :: _ -> Prim (Print, args), None
+  | _ -> assert false
 
-and propagation_named (named : named) (env : env_domain) : named * value_domain =
-    match named with
-    | Var var' -> Var var', get env var'
-    | Prim (prim, args) -> propagation_prim prim args env
-    | Tuple vars -> Tuple vars, Tuple_domain (List.map (fun var' -> get env var') vars)
-    | Get (var', pos) -> begin match get env var' with
-      | Tuple_domain values -> Get (var', pos), List.nth values pos
-      | _ -> failwith "invalid type"
-      end
-    | Closure (k, var') -> Closure (k, var'), Tuple_domain [Pointer_domain (Pointer_domain.singleton k); get env var']
-    (*TODO*)
-    | Environment vars -> Tuple vars, Tuple_domain (List.map (fun var' -> get env var') vars)
-    | Tag x -> Tag x, Int_domain (Int_domain.singleton x)
-    | Constructor (tag, environment_id) -> Constructor (tag, environment_id), Tuple_domain [Pointer_domain (Pointer_domain.singleton tag); get env environment_id]
+and propagation_named (named : Cps.named) (env: (address * Values.t) list) (allocations: value_domain Allocations.t): named * value_domain option =
+match named with
+| Var var' -> Var var', get env var' allocations
+| Prim (prim, args) -> propagation_prim prim args env allocations
+| Tuple vars -> Tuple vars, Some (Tuple_domain (map_args2 vars env))
+| Get (var', pos) -> begin
+    match get env var' allocations with
+    | Some (Tuple_domain values) -> Get (var', pos), join_allocs (List.nth values pos) allocations
+    | None -> Get (var', pos), None
+    | _ -> assert false
+  end
+| Closure (k, values) -> Closure (k, values), Some (Closure_domain (Analysis.Closures.singleton k (map_args2 values env)))
+(* TODO *)
+| Constructor (tag, environment) -> Constructor (tag, environment), Some (Closure_domain (Analysis.Closures.singleton tag (map_args2 environment env)))
 
-and propagation (cps : expr) (env: env_domain) (conts : cont) : expr =
+and propagation (cps : Cps.expr) (env: (pointer * Analysis.Values.t) list) (allocations: value_domain Allocations.t): expr =
   match cps with
-  | Let (var, named, expr) -> let named', value = propagation_named named env in Let (var, named', propagation expr ((var, value)::env) conts)
-  | Apply_cont (k', args, stack) -> Apply_cont (k', args, stack)
-  | If (var, matchs, (kf, argsf), stack) ->
-    if has env var then begin
-      match get env var with
-      | Int_domain i when Int_domain.is_singleton i -> begin
+  | Let (var, named, expr) -> begin
+      let named, value = propagation_named named env allocations in
+      match value with
+      | Some value' -> Let (var, named, propagation expr ((var, Values.singleton var)::env) (Allocations.add var value' allocations))
+      | None -> Let (var, named, propagation expr ((var, Values.empty)::env) allocations)
+    end
+  | Apply_cont (k', args) -> Apply_cont (k', args)
+  | If (var, matchs, (kf, argsf)) -> begin
+      match get env var allocations with
+      | Some (Int_domain i) when Int_domain.is_singleton i -> begin
         match List.find_opt (fun (n', _, _) -> Int_domain.get_singleton i = n') matchs with
-        | Some (_, kt, argst) -> Apply_cont (kt, argst, stack)
-        | None -> Apply_cont (kf, argsf, stack)
+        | Some (_, kt, argst) -> Apply_cont (kt, argst)
+        | None -> Apply_cont (kf, argsf)
         end
-      | Int_domain _ -> If (var, matchs, (kf, argsf), stack)
-      | _ -> failwith "invalid type"
-    end else If (var, matchs, (kf, argsf), stack)
+      | Some (Int_domain _) | None -> If (var, matchs, (kf, argsf))
+      | _ -> assert false
+    end
+  | Match_pattern (var, matchs, (kf, argsf)) -> begin
+    match get env var allocations with
+    | Some (Closure_domain clos) -> Match_pattern (var, List.filter (fun (n, _, _) -> List.exists (fun (n', _) -> n = n') (Analysis.Closures.bindings clos)) matchs, (kf, argsf))
+    | None -> Match_pattern (var, matchs, (kf, argsf))
+    | _ -> assert false
+  end
   | Return x -> Return x
-  | Call (x, args, stack) when has env x -> begin
-    match get env x with
-    | Pointer_domain k -> if Pointer_domain.is_singleton k then Apply_cont (Pointer_domain.get_singleton k, args, stack) else Call (x, args, stack)
-    | _ -> failwith "invalid type" end
-  | Call (x, args, stack) -> Call (x, args, stack)
+  | Call (x, args, stack) -> begin
+      match get env x allocations with
+      | Some (Pointer_domain k) -> if Pointer_domain.is_singleton k then Call_direct (Pointer_domain.get_singleton k, x, args, stack) else Call_indirect (x, args, stack)
+      | Some _ | None -> Call_indirect (x, args, stack)
+    end
 
-and propagation_cont (cps : cont) (conts : cont) map : cont =
+and propagation_cont (cps: Cps.cont) (conts: Cps.cont) (map: (value_domain Allocations.t * Analysis.Values.t list) Analysis.Analysis.t): cont =
   match cps with
-  | Let_cont (k', args', e1, e2) -> let e1' = if Analysis.Analysis.mem k' map then
-    let env = (Analysis.Analysis.find k' map) in
-    propagation e1 (Analysis.map_values args' env) conts else e1 in
-    let e2' = propagation_cont e2 conts map in
-    Let_cont (k', args', e1', e2')
+  | Let_cont (k', args', e1, e2) -> begin
+      let allocations, env = if Analysis.Analysis.mem k' map then Analysis.Analysis.find k' map else Analysis.Allocations.empty, List.map (fun _ -> Analysis.Values.empty) args' in
+      let e1' = propagation e1 (Analysis.map_values args' env) allocations in
+      let e2' = propagation_cont e2 conts map in
+      Let_cont (k', args', e1', e2')
+    end
+  | Let_clos (k', env', args', e1, e2) -> begin
+      let allocations, env = if Analysis.Analysis.mem k' map then Analysis.Analysis.find k' map else Analysis.Allocations.empty, List.map (fun _ -> Analysis.Values.empty) (env'@args') in
+      let e1' = propagation e1 (Analysis.map_values (env'@args') env) allocations in
+      let e2' = propagation_cont e2 conts map in
+      Let_cont (k', args', e1', e2')
+    end
   | End -> End
 ;;
